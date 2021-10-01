1290dd5684d929e8cbec3ca85d415d825637399a
Aryan9301 <36062441+Aryan9301@users.noreply.github.com>
2021-10-02 00:28:54 +0800

    Update Experiment9.java
    Added remaining part of the code along with details

--- Experiment9.java
+++ Experiment9.java
@@ -17,3 +17,63 @@ public class MainActivity extends AppCompatActivity {
     public static final String mypref = "mypref";
     public static final String Name = "namekey";
     public static final String Email = "emailkey";
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (TextView) findViewById(R.id.name);
+        email = (TextView) findViewById(R.id.email);
+        btnSave = findViewById(R.id.save);
+        btnRetr = findViewById(R.id.retrieve);
+        btnClear = findViewById(R.id.clear);
+        sharedpref = getSharedPreferences(mypref, Context.MODE_PRIVATE);
+        if(sharedpref.contains(Name)){
+            name.setText(sharedpref.getString(Name, ""));
+        }
+        if(sharedpref.contains(Email)){
+            email.setText(sharedpref.getString(Email, ""));
+        }
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = (TextView) findViewById(R.id.name);
+                email = (TextView) findViewById(R.id.email);
+                name.setText("");
+                email.setText("");
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = name.getText().toString();
+                String e = email.getText().toString();
+                SharedPreferences.Editor editor = sharedpref.edit();
+                editor.putString(Name, n);
+                editor.putString(Email, e);
+                editor.commit();
+            }
+        });
+
+        btnRetr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = (TextView) findViewById(R.id.name);
+                email = (TextView) findViewById(R.id.email);
+                sharedpref = getSharedPreferences(mypref, Context.MODE_PRIVATE);
+                if (sharedpref.contains(Name)) {
+                    name.setText(sharedpref.getString(Name, ""));
+                }
+                if (sharedpref.contains(Email)) {
+                    email.setText(sharedpref.getString(Email, ""));
+                }
+
+            }
+        });
+    }
+
+
+
+}
+
